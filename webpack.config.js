4e75c4496211673d4c1b9bbd8b867d090e403e80
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-03-23 22:58:19 +0400

    fix: implement repository setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
@@ -13,14 +13,20 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['ts', 'tsx', '.js'],
     },
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
+        // filename: 'index.js',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new DotenvWebpackPlugin(),
